73eaf0c2d4092a4a358fcbe76f16f7da50df762f
sushuai <sushuai@didichuxing.com>
2017-07-13 20:03:47 +0800

    修改

--- tank/src/main/java/com/sus/tank/warehouse/TipsPresenter.java
+++ tank/src/main/java/com/sus/tank/warehouse/TipsPresenter.java
@@ -5,11 +5,10 @@ import android.os.Bundle;
 
 import com.sus.tankcommon.warehouse.tips.presenter.AbsTipsPresenter;
 
-
 /**
- * Created by chensi on 2017/6/27.
+ * Created by sushuai
+ * Date: 17/7/13
  */
-
 public class TipsPresenter extends AbsTipsPresenter {
 
     public TipsPresenter(Context context) {
